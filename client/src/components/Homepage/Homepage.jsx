9eea786657c57daaceb3d792615b91fcd41f921f
CyberMage7 <technician9012@gmail.com>
2024-04-20 15:52:47 +0530

    Added Timeline

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Body from './Body/Body'
+import Timeline from './Timeline/Timeline'
 
 function Homepage() {
   return (
     <div>
       <Navbar />
       <Body />
+      <Timeline />
     </div>
   )
 }
