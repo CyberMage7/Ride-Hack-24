8680e7526bb5cfcd77820eb9932365d796ae168b
CyberMage7 <technician9012@gmail.com>
2024-04-20 14:48:31 +0530

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login"; 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path ="/" element={<Homepage />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
       </Routes>
