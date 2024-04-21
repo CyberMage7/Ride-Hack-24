2719a2b57a5792190aff8665394e0e9df1a13884
rmall2003 <rmcoding.2003@gmail.com>
2024-04-21 11:55:11 +0530

    Added Footer to All Pages

--- client/src/components/Homepage/Homepage.jsx
+++ client/src/components/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar/Navbar'
 import Body from './Body/Body'
 import Timeline from './Timeline/Timeline'
+import Footer from "../footer/Footer";
 
 function Homepage() {
   return (
@@ -9,6 +10,7 @@ function Homepage() {
       <Navbar />
       <Body />
       <Timeline />
+      <Footer />
     </div>
   )
 }
